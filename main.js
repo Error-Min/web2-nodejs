9da0cc12839d6c9ff00f503711a65706b21c7dc8
Error-Min <smgm030@gmail.com>
2020-09-18 00:25:39 +0900

    2020-09-18 node js

--- main.js
+++ main.js
@@ -1,20 +1,73 @@
 var http = require('http');
 var fs = require('fs');
-var url = require('url'); // 리콰이어  = 요구하다 http를 요구하다?
+var url = require('url');
 
 var app = http.createServer(function(request, response) {
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
+    var pathname = url.parse(_url, true).pathname; //패스 네임이 있는 곳이라면 if문 안에 작동한다
 
-    console.log(queryData.id);
-    if (_url == '/') {
-        _url = '/index.html';
+
+
+    if (pathname === '/') {
+        if (queryData.id === undefined) {
+            fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description) { // 파일 데이터를 가지고 와서 데이터를 뿌린다.
+                var title = "Welcome";
+                var description = "Hello, Node.js";
+                var template = `
+                <!doctype html>
+                <html>
+                <head>
+                  <title>WEB1 - ${title}</title>
+                  <meta charset="utf-8">
+                </head>
+                <body>
+                  <h1><a href="/">WEB</a></h1>
+                  <ul>
+                    <li><a href="/?id=HTML">HTML</a></li>
+                    <li><a href="/?id=CSS">CSS</a></li>
+                    <li><a href="/?id=JavaScript">JavaScript</a></li>
+                  </ul>
+                  <h2>${title}</h2>
+                  <p>${description}</p>
+                </body>
+                </html>
+                `;
+                response.writeHead(200);
+                response.end(template);
+            });
+        } else {
+            fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description) { // 파일 데이터를 가지고 와서 데이터를 뿌린다.
+                var title = queryData.id;
+                var template = `
+                <!doctype html>
+                <html>
+                <head>
+                  <title>WEB1 - ${title}</title>
+                  <meta charset="utf-8">
+                </head>
+                <body>
+                  <h1><a href="/">WEB</a></h1>
+                  <ul>
+                    <li><a href="/?id=HTML">HTML</a></li>
+                    <li><a href="/?id=CSS">CSS</a></li>
+                    <li><a href="/?id=JavaScript">JavaScript</a></li>
+                  </ul>
+                  <h2>${title}</h2>
+                  <p>${description}</p>
+                </body>
+                </html>
+                `;
+                response.writeHead(200);
+                response.end(template);
+            });
+        }
+    } else { // 패스 네임이 없으면 이 부분이 작동한다.
+        response.writeHead(404);
+        response.end('Not found');
     }
-    if (_url == '/favicon.ico') {
-        return response.writeHead(404);
-    }
-    response.writeHead(200);
-    response.end(queryData.id); // 경로에 해당하는 파일을 읽어서 가지고 온다.
+
+
 
 });
 app.listen(3000);
